62e34b410f37371b20eb77b0582ef49a08e311e3
Alexis BENOIT <alex@noesya.coop>
2026-02-23 13:21:22 +0100

    better image control

--- assets/js/osuny-masonry.js
+++ assets/js/osuny-masonry.js
@@ -12,11 +12,11 @@ window.osuny.masonry = {
             return;
         }
 
-        this.images = this.container.querySelectorAll('img');
-
         if (this.container.children.length < 4) {
             return;
         }
+
+        this.images = this.container.querySelectorAll('img');
 
         this.setup();
         this.update();
@@ -32,7 +32,7 @@ window.osuny.masonry = {
     },
     bind: function () {
         'use strict';
-        window.addEventListener('resize', this.throttleResize.bind(this));
+        window.addEventListener('resize', this.resize.bind(this, null));
         this.container.addEventListener('change', this.update.bind(this));
     },
     update: function () {
@@ -41,25 +41,45 @@ window.osuny.masonry = {
         this.children = Array.prototype.slice.call(this.container.children);
 
         this.images.forEach(function (image) {
-            image.addEventListener('load', this.resize.bind(this));
+            this.checkImageState(image);
         }.bind(this));
 
         this.throttleResize();
     },
-    resize: function () {
+    resize: function (image) {
         'use strict';
+
         var maximumY = 0,
             offset = this.container.offsetTop - 10;
+
         this.container.style.height = 1000000 + 'px';
+
         this.children.forEach(function (child) {
             maximumY = Math.max(maximumY, child.offsetTop + child.offsetHeight - offset);
         });
+
+        if (image) {
+            this.onImageLoad(image);
+        }
+
         this.container.style.height = maximumY + 'px';
+    },
+    checkImageState: function (image) {
+        'use strict';
+        if (image.complete) {
+            this.onImageLoad(image);
+        } else {
+            image.addEventListener('load', this.resize.bind(this, image));
+        }
+    },
+    onImageLoad: function (image) {
+        'use strict';
+        image.classList.add('is-loaded');
     },
     throttleResize: function () {
         'use strict';
         clearTimeout(this.resizeTimeout);
-        this.resizeTimeout = setTimeout(this.resize.bind(this), 300);
+        this.resizeTimeout = setTimeout(this.resize.bind(this, null), 100);
     }
 };
 
